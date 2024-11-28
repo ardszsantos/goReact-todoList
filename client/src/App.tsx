aa225745a88085954f2a74908c3ebfd13ab8ddaa
ardszsantos <lokgamer01@gmail.com>
2024-11-28 13:55:49 -0300

    baseurl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import { Container, Stack } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+
+export const BASE_URL = "http://localhost:5000/api"
 
 function App() {
-
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* <TodoForm/>
-        <TodoForm/> */}
+        <TodoForm/>
+        <TodoList/>
       </Container>
     </Stack>
   )
